010e37321f55bd1682000c322d41d5cb238490ec
DK\obr <obr@sitecore.net>
2018-03-29 10:56:16 +0300

    Inheritance is changed from CustomField to ReferenceField in order to prevent some issues which can appear when we perform a check as following (is ReferenceField) For example in Sitecore.ContentSearch.FieldReaders.LookupFieldReader, Sitecore.ContentSearch

--- src/Sitecore.Support.205771/ReferenceField.cs
+++ src/Sitecore.Support.205771/ReferenceField.cs
@@ -5,84 +5,13 @@
     using Sitecore.Data.Items;
     using Sitecore.Diagnostics;
     using Sitecore.Links;
-    public class ReferenceField : CustomField
+    public class ReferenceField : Sitecore.Data.Fields.ReferenceField
     {
-        #region Original code
         public ReferenceField(Field innerField) : base(innerField)
         {
             Assert.ArgumentNotNull(innerField, "innerField");
         }
 
-        public Sitecore.Data.Database Database =>
-         (this.GetDatabase() ?? base.InnerField.Database);
-
-        public string Path
-        {
-            get { return base.Value; }
-            set
-            {
-                Assert.ArgumentNotNull(value, "value");
-                base.Value = value;
-            }
-        }
-
-        public ID TargetID
-        {
-            get
-            {
-                Item item = this.Database.Items[this.Path];
-                if (item == null)
-                {
-                    return ID.Null;
-                }
-                return item.ID;
-            }
-        }
-
-        public Item TargetItem =>
-            this.Database.Items[this.Path];
-
-        public void Clear()
-        {
-            base.Value = string.Empty;
-        }
-
-        public static implicit operator ReferenceField(Field field)
-        {
-            if (field == null)
-            {
-                return null;
-            }
-            return new ReferenceField(field);
-        }
-
-        public override void RemoveLink(ItemLink itemLink)
-        {
-            Assert.ArgumentNotNull(itemLink, "itemLink");
-            if (itemLink.TargetItemID == this.TargetID)
-            {
-                this.Clear();
-            }
-        }
-
-        public override void ValidateLinks(LinksValidationResult result)
-        {
-            Assert.ArgumentNotNull(result, "result");
-            if (!string.IsNullOrEmpty(this.Path) && (this.Path != "{00000000-0000-0000-0000-000000000000}"))
-            {
-                Item targetItem = this.TargetItem;
-                if (targetItem != null)
-                {
-                    result.AddValidLink(targetItem, this.Path);
-                }
-                else
-                {
-                    result.AddBrokenLink(this.Path);
-                }
-            }
-        }
-        #endregion
-        #region Modified code
         public override void Relink(ItemLink itemLink, Item newLink)
         {
             Assert.ArgumentNotNull(itemLink, "itemLink");
@@ -92,7 +21,6 @@
                 this.Path = newLink.ID.ToString();
             }
         }
-        #endregion
     }
 }
 
